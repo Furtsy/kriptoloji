50531a541def5a1135ecdcb796ae2b5c80097b77
Furtsy <43937364+Furtsy@users.noreply.github.com>
2021-12-08 22:28:37 +0300

    Update main.java

--- md5/main.java
+++ md5/main.java
@@ -5,7 +5,7 @@ public class main {
 		System.out.println("Şifrelenmemiş Hali: " + plainText);
 		MD5 md5Object = new MD5();
 		String digesText = md5Object.hash(plainText);
-		System.out.println("MD5 ile şifrelenmiş hali" + digesText);
+		System.out.println("MD5 ile şifrelenmiş hali: " + digesText);
 	}
 
 }
