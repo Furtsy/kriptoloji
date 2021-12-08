fc7dfc29eeb8dba7fec1ed63fbd9d817f66a410f
Furtsy <43937364+Furtsy@users.noreply.github.com>
2021-12-08 22:19:33 +0300

    Create main.java

---
+++ md5/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+	public static void main(String[] args) {
+		String plainText = "furtsy örnek";
+		System.out.println("Şifrelenmemiş Hali: " + plainText);
+		MD5 md5Object = new MD5();
+		String digesText = md5Object.hash(plainText);
+		System.out.println("MD5 ile şifrelenmiş hali" + digesText);
+	}
+
+}
