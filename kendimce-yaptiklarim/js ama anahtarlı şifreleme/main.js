6402e55bfeb813edb0e3c1c0844bb139c1f2cc2e
Furtsy <43937364+Furtsy@users.noreply.github.com>
2021-12-08 23:17:18 +0300

    Create main.js

---
+++ kendimce-yaptiklarim/js ama anahtarlı şifreleme/main.js
@@ -0,0 +1,30 @@
+const oluştur = (anahtar, text) => {
+  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+  const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+  const applySaltToChar = (code) => textToChars(anahtar).reduce((a, b) => a ^ b, code);
+
+  return text
+    .split("")
+    .map(textToChars)
+    .map(applySaltToChar)
+    .map(byteHex)
+    .join("");
+};
+
+const çöz = (anahtar, encoded) => {
+  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+  const applySaltToChar = (code) => textToChars(anahtar).reduce((a, b) => a ^ b, code);
+  return encoded
+    .match(/.{1,2}/g)
+    .map((hex) => parseInt(hex, 16))
+    .map(applySaltToChar)
+    .map((charCode) => String.fromCharCode(charCode))
+    .join("");
+};
+
+
+// şifre oluştur
+console.log(oluştur("furstea", "merhaba dünya")) // -> 0f07100a03000342069e0c1b03
+
+// şifreyi çöz
+console.log(çöz("furstea", "0f07100a03000342069e0c1b03")) // -> merhaba dünya
