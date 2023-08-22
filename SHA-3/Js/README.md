4bc92d8d3346896a818788cf92cda56e9fbdaa59
Furtsy <43937364+Furtsy@users.noreply.github.com>
2023-08-22 13:51:39 +0300

    adding js to sha3

---
+++ SHA-3/Js/README.md
@@ -0,0 +1,6 @@
+# Çıktı
+```
+Metnin şifrelenmemiş hali: Furtsy denemee
+SHA3 ile şifrelenmiş hali:
+4387ceb4453475bb521f102b40720d4028a8bdff0410aafe00de57b2ae35b7a6
+```
---
+++ SHA-3/Js/main.js
@@ -0,0 +1,19 @@
+const crypto = require('crypto');
+
+class SHA3 {
+  digest(text, size) {
+    const digestObject = crypto.createHash(`sha3-${size}`);
+    digestObject.update(text, 'utf8');
+    return digestObject.digest('hex');
+  }
+}
+
+const length = 256;
+const plainText = 'Furtsy denemee';
+
+console.log(`Şifrelenmemiş hali: ${plainText}`);
+
+const sha3Object = new SHA3();
+const digest = sha3Object.digest(plainText, length);
+
+console.log(`SHA3 ile şifrelenmiş hali:\n${digest}`);
